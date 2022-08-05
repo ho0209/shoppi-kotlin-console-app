074590931589c8ad2022948a8c02b4c225bd2063
박장호 <jangho209@gmail.com>
2022-08-06 01:04:23 +0900

    실습 3-3. Product 객체 추가 및 ProductList 화면이 상품 출력 담당

--- src/main/kotlin/screen/ShoppingCategory.kt
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -17,9 +17,9 @@ class ShoppingCategory {
         if (selectedCategory == "#") {
             // TODO 1. 장바구니 이동
         } else {
-            // TODO 2. 카테고리 상품 목록 보여주기
             if (categories.contains(selectedCategory)){
-                // TODO 2. 카테고리 상품 목록 보여주기
+                val shoppingProductList = ShoppingProductList()
+                shoppingProductList.showProducts(selectedCategory)
             }else{
                 showErrorMessage(selectedCategory)
             }
